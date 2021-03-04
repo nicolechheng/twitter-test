a4b12c6cc339e292bf308a3ace54c51568e09a9a
nicolechheng <71405252+nicolechheng@users.noreply.github.com>
2021-03-04 01:21:17 -0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name="twitter-bot test"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ntc2ju@virginia.edu
+#SBATCH --output="bot.output"
+
+source venv/bin/activate
+
+time python3 scraper.py
