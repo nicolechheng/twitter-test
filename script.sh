5447716a2df8cf4f7c9b904e7426cd879f04a7c6
nicolechheng <71405252+nicolechheng@users.noreply.github.com>
2021-03-09 00:01:53 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,9 @@
 # --- this job will be run on any available node
 # and simply output the node's hostname to
 # my_job.output
-#SBATCH --job-name="Slurm Simple Test Job"
+#SBATCH --job-name="CA data"
 #SBATCH --error="my_job.err"
 #SBATCH --output="my_job.output"
-echo "$HOSTNAME"
+source venv/bin/activate
+
+time python3 scraper.py
